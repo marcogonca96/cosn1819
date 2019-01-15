4724e687e3c3b1c23f7ad0e8de0dd0e1a90687a8
01alex <xanomcr@gmail.com>
2019-01-15 18:04:59 +0000

    added dbs

--- User/UserAuth/settings.py
+++ User/UserAuth/settings.py
@@ -110,7 +110,7 @@ WSGI_APPLICATION = 'UserAuth.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db', 'db.sqlite3'),
     }
 }
 
--- Wishlist/wishlist/settings.py
+++ Wishlist/wishlist/settings.py
@@ -109,7 +109,7 @@ WSGI_APPLICATION = 'wishlist.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db', 'db.sqlite3'),
     }
 }
 
