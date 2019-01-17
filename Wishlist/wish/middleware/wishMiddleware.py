71163bc794b741f7a71af0916e530afdeef3407d
Tiago Cardoso Matias <tiago.matias92@gmail.com>
2019-01-17 21:53:37 +0000

    Add consumer to send email

--- Wishlist/wish/middleware/wishMiddleware.py
+++ Wishlist/wish/middleware/wishMiddleware.py
@@ -14,16 +14,18 @@ class WishMiddleware(object):
     def __call__(self, request):
         # Code to be executed for each request before
         # the view (and later middleware) are called.
-        if 'HTTP_JWT' not in request.META:
-            return HTTP.response(401, 'Falta o token nos headers.', 'Token not present')
+        if request.path.find('/api/wish/users/'):
+            if 'HTTP_JWT' not in request.META:
+                return HTTP.response(401, 'Falta o token nos headers.', 'Token not present')
 
-        try:
-            jwt_encoded = request.META['HTTP_JWT']
-            jwt_decoded = jwt.decode(jwt_encoded, settings.JWT_SECRET, algorithm=settings.JWT_ALGORITHM)
-        except jwt.ExpiredSignatureError:
-            return HTTP.response(401, 'Token Expired', 'Token not valid.')
-        except DecodeError:
-            return HTTP.response(401, 'Token não é válido.', 'Token not valid.')
+            try:
+                jwt_encoded = request.META['HTTP_JWT']
+                jwt_decoded = jwt.decode(jwt_encoded, settings.JWT_SECRET, algorithm=settings.JWT_ALGORITHM)
+            except jwt.ExpiredSignatureError:
+                return HTTP.response(401, 'Token Expired', 'Token not valid.')
+            except DecodeError:
+                return HTTP.response(401, 'Token não é válido.', 'Token not valid.')
 
-        request.USER_ID = jwt_decoded['user_id']
+            request.USER_ID = jwt_decoded['user_id']
+            request.USER_EMAIL = jwt_decoded['email']
         return self.get_response(request)
--- consumer-service/consumer.py
+++ consumer-service/consumer.py
@@ -1,7 +1,9 @@
+import re
 import pika
 import time
+import requests
 
-#delay to start rabbitmq
+# delay to start rabbitmq
 time.sleep(20)
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
@@ -10,11 +12,29 @@ channel = connection.channel()
 channel.queue_declare(queue='wish', durable=True)
 print(' [*] Waiting for messages. To exit press CTRL+C')
 
+
+def request_user_emails(categories):
+    category_list = ','.join(categories)
+    response = requests.request(method='GET', url='wishlist:8000/api/wish/users/?categories='+category_list)
+    if response.status_code == 200:
+        return response.json()
+
+
+def send_email(email, context):
+    pass
+
+
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
+    categories =  re.findall(r'\d+', body)
+    emails = request_user_emails(categories)
+    context = 'New Video Added'
+    for email in emails:
+        send_email(email, context)
     time.sleep(body.count(b'.'))
     print(" [x] Done")
     ch.basic_ack(delivery_tag = method.delivery_tag)
+
 
 channel.basic_qos(prefetch_count=1)
 channel.basic_consume(callback,
